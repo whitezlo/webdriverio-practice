9832c92f5c194b35a7facc5cdac7c6e063567566
Ilie Diacov <iliadiacov@mail.ru>
2017-04-10 14:09:16 -0400

    See one more translation

--- test/specs/for-your-health.js
+++ test/specs/for-your-health.js
@@ -11,10 +11,17 @@ describe('webdriver.io page', function() {
     it('should have the right translation', function () {
         var textRussian="На Здоровье!";
         var input='#tw-source-text-ta';
+        var oneMoreTranslation='div=1 more translation';
         var textEnglish="Bless you!";
+        var secondaryTranslation='.tw-bilingual-translation';
 
         browser.url('https://www.google.ca/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=google+translate+russian+to+english');
-        browser.waitForExist(input, 5000)
+        browser.waitForExist(input, 5000);
         browser.setValue(input, textRussian);
+
+        browser.waitForExist(oneMoreTranslation, 5000);
+        browser.click(oneMoreTranslation);
+
+        browser.pause(5000);
     });
 });
